caccd180767822bec417c90202ca8b74137cae73
MassHesteria <brad.hester@gmail.com>
2024-12-18 07:42:11 -0600

    Added server function

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { getServerTime } from "~/lib/getServerTime"
 
 export const Route = createFileRoute('/')({
   component: Home,
-  //loader: async () => await getCount(),
+  loader: async () => await getServerTime()
 })
 
 function Home() {
+  const state = Route.useLoaderData();
   return (
     <>
       <div>hello tanstack</div>
       <div className="text-lg font-bold text-red-700 pl-5">update test</div>
+      <div>{state}</div>
     </>
   )
 }
